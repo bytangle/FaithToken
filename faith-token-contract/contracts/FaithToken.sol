3bbb5089798f7970b9f57cee9f8aed6afbab63c7
Bytangle <dev.bytangle@gmail.com>
2022-08-22 14:41:23 +0100

    Apply comments and implement more functions

--- faith-token-contract/contracts/FaithToken.sol
+++ faith-token-contract/contracts/FaithToken.sol
@@ -16,18 +16,52 @@ import "./ERC20Interface.sol";
      uint private totalSupply_;
      string private name_;
      string private symbol_;
+     uint private decimals_;
 
-     constructor(string memory _name, string memory _symbol, uint _initialOwnerBal) {
+     constructor(
+         string memory _name, 
+         string memory _symbol, 
+         uint _decimals,  
+         uint _initialOwnerBal) {
          name_ = _name;
          symbol_ = _symbol;
          balances_[msg.sender] = _initialOwnerBal;
      }
 
+    /**
+     * @notice returns the name of the token
+     * @dev getter function for name property
+     * @return name of the the token [string]
+     */
      function name() public view returns (string memory) {
          return name_;
      }
 
+    /**
+     * @notice returns the token symbol
+     * @dev getter function for symbol property
+     * @return symbol of token [string]
+     */
      function symbol() public view returns (string memory) {
          return symbol_;
+     }
+
+    /**
+     * @notice returns total token supply
+     * @dev getter function for totalSupply property
+     * @return total supply of token
+     */
+     function totalSupply() public view returns (uint) {
+         return totalSupply_;
+     }
+
+    /**
+     * @dev returns the balance stored using the provided address
+     * @notice returns the balance of the owner using the provided address
+     * @param _owner address to be used to lookup the balance
+     * @return balance retrieved using the provided address
+     */
+     function balanceOf(address _owner) public view returns (uint) {
+         return balances_[_owner];
      }
  }
